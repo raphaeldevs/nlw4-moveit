5fb6779b42797904acd2dbfb4c16db1cfad88599
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-02-28 03:12:39 -0300

    create ranking page

---
+++ src/pages/ranking.tsx
@@ -0,0 +1,58 @@
+import Head from 'next/head'
+
+import { AsideBar } from '../components/AsideBar'
+
+import styles from '../styles/pages/Ranking.module.css'
+
+export default function Ranking() {
+  return (
+    <>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
+      <div className={styles.wrapper}>
+        <AsideBar />
+
+        <div className={styles.container}>
+          <h1>Leaderboard</h1>
+
+          <table>
+            <thead>
+              <tr>
+                <th>Posição</th>
+                <th>Usuário</th>
+                <th>Desafios</th>
+                <th>Experiência</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>
+                  <img
+                    src="https://github.com/raphaeldevs.png"
+                    alt="Raphael Corrêa"
+                  />
+                  <div>
+                    <strong>Raphael Devs</strong>
+                    <span>
+                      <img src="/icons/level.svg" alt="Level" /> Level 19
+                    </span>
+                  </div>
+                </td>
+                <td>
+                  <strong>127</strong> completados
+                </td>
+                <td>
+                  <strong>108.000</strong> xp
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  )
+}
