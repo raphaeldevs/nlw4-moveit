11eeaeccd8862d21533b90549c573bc4340f90fc
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-01 17:27:17 -0300

    add title in login screen

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,26 +1,34 @@
+import Head from 'next/head'
+
 import styles from '../styles/pages/Login.module.css'
 
 export default function Login() {
   return (
-    <div className={styles.container}>
-      <form>
-        <img src="logo-full-white.svg" alt="Logo do Move.it" />
+    <>
+      <Head>
+        <title>Bem vindo ao move.it</title>
+      </Head>
 
-        <h1>Bem-vindo</h1>
-        <p>
-          <img src="icons/github.svg" alt="Logo do GitHub" />
-          Faça login com seu Github <br /> para começar
-        </p>
+      <div className={styles.container}>
+        <form>
+          <img src="logo-full-white.svg" alt="Logo do Move.it" />
 
-        <fieldset>
-          <input
-            className={styles.inputSearch}
-            type="text"
-            placeholder="Digite seu username"
-          />
-          <input className={styles.inputSubmit} name="submit" type="submit" />
-        </fieldset>
-      </form>
-    </div>
+          <h1>Bem-vindo</h1>
+          <p>
+            <img src="icons/github.svg" alt="Logo do GitHub" />
+            Faça login com seu Github <br /> para começar
+          </p>
+
+          <fieldset>
+            <input
+              className={styles.inputSearch}
+              type="text"
+              placeholder="Digite seu username"
+            />
+            <input className={styles.inputSubmit} name="submit" type="submit" />
+          </fieldset>
+        </form>
+      </div>
+    </>
   )
 }
