107d636c9df94290da1fe7a3f98b0e4504e90a41
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-03 02:21:47 -0300

    connect with mongodb

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -2,7 +2,28 @@ import axios from 'axios'
 
 import { NextApiRequest, NextApiResponse } from 'next'
 
-const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = process.env
+import { MongoClient, Db } from 'mongodb'
+
+const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, MONGODB_URI } = process.env
+
+let cachedDatabase: Db
+
+async function connectToDatabase(uri: string) {
+  if (cachedDatabase) return cachedDatabase
+
+  const client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+
+  const databaseName = new URL(uri).pathname.substr(1)
+
+  const database = client.db(databaseName)
+
+  cachedDatabase = database
+
+  return database
+}
 
 async function getUserAccessToken(code: string) {
   const { data: apiResponse } = await axios.post('https://github.com/login/oauth/access_token', {}, {
@@ -42,11 +63,19 @@ async function getUserData(access_token: string) {
 export default async (request: NextApiRequest, response: NextApiResponse) => {
   const { code } = request.query
 
+  const database = await connectToDatabase(MONGODB_URI)
+
+  const collection = database.collection('users')
+
   const access_token = await getUserAccessToken(String(code))
 
   const user = await getUserData(access_token)
 
-  console.log(user)
+  const userAlreadyExists = await collection.findOne(user)
 
-  return response.json({ ok: true })
+  if (!userAlreadyExists) {
+    await collection.insertOne(user)
+  }
+
+  return response.json({ user: userAlreadyExists ? "exits" : "no exists" })
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,50 @@
 import Head from 'next/head'
 
+import { GetServerSideProps } from 'next'
+
+import { useRouter } from 'next/router'
+
+import { ChangeEvent, FormEvent, useState } from 'react'
+
 import styles from '../styles/pages/Login.module.css'
 
-export default function Login() {
+interface LoginProps {
+  client_id: string
+}
+
+export default function Login({ client_id }: LoginProps) {
+  const history = useRouter()
+
+  const [canSubmit, setCanSubmit] = useState(false)
+  const [username, setUsername] = useState('')
+
+  function handleInputChange({ target: input }: ChangeEvent<HTMLInputElement>) {
+    const { value: username } = input
+
+    if (username.length) {
+      setUsername(username)
+
+      return setCanSubmit(true)
+    }
+
+    setCanSubmit(false)
+  }
+
+  function handleFormSubmit(event: FormEvent) {
+    event.preventDefault()
+    
+    const urlLoginParams = new URLSearchParams({
+      client_id,
+      login: username,
+      scope: 'user:email'
+    }).toString()
+
+    const loginUrl = 
+      `https://github.com/login/oauth/authorize?${urlLoginParams}`
+
+    history.push(loginUrl)
+  }
+
   return (
     <>
       <Head>
@@ -10,7 +52,7 @@ export default function Login() {
       </Head>
 
       <div className={styles.container}>
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <img src="logo-full-white.svg" alt="Logo do Move.it" />
 
           <h1>Bem-vindo</h1>
@@ -23,12 +65,38 @@ export default function Login() {
             <input
               className={styles.inputSearch}
               type="text"
+              name="username"
               placeholder="Digite seu username"
+              onChange={handleInputChange}
+              required
             />
-            <input className={styles.inputSubmit} name="submit" type="submit" />
+
+            {canSubmit ? (
+              <input
+                className={`${styles.inputSubmit} ${styles.canSubmit}`}
+                type="submit"
+              />
+            ) : (
+              <input
+                className={styles.inputSubmit}
+                type="submit"
+              />
+            )}
           </fieldset>
         </form>
       </div>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const { GITHUB_CLIENT_ID } = process.env
+
+  console.log(context.req.headers)
+
+  return {
+    props: {
+      client_id: GITHUB_CLIENT_ID
+    }
+  }
 }
