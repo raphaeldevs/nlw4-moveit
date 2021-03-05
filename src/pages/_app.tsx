8a819c46946344a897ece09a70e79340d529efc8
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-04 21:45:21 -0300

    Revert "add auth provider in _app"
    This reverts commit 077498c17126899789bf8a8566ffe389a1360010.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/global.css'
 
+import { AuthProvider } from '../contexts/AuthContext'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
 
 export default MyApp
