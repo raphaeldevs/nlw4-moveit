3845ed2cbe500954323babae8ae155cc4f26f563
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-04 21:44:47 -0300

    validate user token

---
+++ src/pages/api/auth/validate.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import jwt from 'jsonwebtoken'
+
+export default async (request: NextApiRequest, response: NextApiResponse) => {  
+  const { APP_SECRET } = process.env
+
+  if (request.method === 'POST') {
+    const { token } = request.cookies
+
+    if (!token) return response.json({ isAuthenticated: false })
+
+    const isValidToken = jwt.verify(token, APP_SECRET)
+
+    return response.json({ isAuthenticated: isValidToken })
+  }
+}
